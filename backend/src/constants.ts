e9457bf5c514aa8e3c60c6265f647658b24e5895
Denis Kole <denis.kole4@gmail.com>
2022-12-24 11:40:21 +0200

    Rearrange constants of CSV headers

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,3 +1,28 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 
 export const ROWS_PER_PAGE: number = 20;
+export const JOURNEY_CSV_HEADER: Array<string> = [
+    "departure_time",
+    "return_time",
+    "departure_id",
+    "departure_name",
+    "return_id",
+    "return_name",
+    "distance",
+    "duration"
+];
+export const STATION_CSV_HEADER: Array<string> = [
+    "fid",
+    "id",
+    "nimi",
+    "namn",
+    "name",
+    "osoite",
+    "adress",
+    "kaupunki",
+    "stad",
+    "operator",
+    "capacity",
+    "longitude",
+    "latitude"
+];
