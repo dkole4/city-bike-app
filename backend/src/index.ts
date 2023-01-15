9e72b2f3179be8aac558d7025168be8a50f3abeb
Denis Kole <denis.kole4@gmail.com>
2023-01-15 16:34:32 +0200

    Add cors package to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import { config } from "dotenv";
 config();
 
 import express from "express";
+import cors from "cors";
 
 import { AppDataSource } from "./data-source";
 import { router as stationRouter } from "./routes/station.routes";
@@ -13,10 +14,11 @@ import { router as importRouter } from "./routes/import.routes";
 const main = async () => {
     const app: express.Application = express();
 
-    app.use(express.json());
-
     AppDataSource.initialize()
         .catch((err) => console.error(err));
+    
+    app.use(express.json());
+    app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
     // Add routes to the app.
     app.use(stationRouter);
