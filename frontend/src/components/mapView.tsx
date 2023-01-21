16b90743d83947ccbb2d414c905dc5e3c5a50dfd
Denis Kole <denis.kole4@gmail.com>
2023-01-22 00:11:34 +0200

    Add missing location message to mapView if location is unknown

--- frontend/src/components/mapView.tsx
+++ frontend/src/components/mapView.tsx
@@ -2,16 +2,29 @@ import React from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 interface MapViewProps {
-    longitude: number;
-    latitude: number;
+    lng: number;
+    lat: number;
 };
 
-const MapView: React.FC<MapViewProps> = ({ longitude, latitude }) => {
-    const location = { lat: latitude, lng: longitude };
+const MapView: React.FC<MapViewProps> = (location) => {
+    // Display a message about missing location data to user.
+    if (!location.lat && !location.lng) {
+        return (
+            <p style={{textAlign: "center"}}>
+                Location of the station is unknown.
+            </p>
+        );
+    }
+
+    // Zoom of the map view.
     const zoom: number = 12;
 
     return (
-        <MapContainer style={{ height: "50vh" }} center={location} zoom={zoom}>
+        <MapContainer
+            style={{ height: "50vh" }}
+            center={location}
+            zoom={zoom}
+        >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
